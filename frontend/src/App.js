ea30e2852f8471e2d5dac3359876c57a2a7b5d63
Teknath <jhateknath64@gmail.com>
2023-01-02 20:19:01 +0530

    Done with landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import LandingPage from "./screens/LandingPage/LandingPage";
 
-function App() {
-  return (
-    <div className="App">
-      Notes Keeper
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Header />
+    <main >
+      <LandingPage />
+    </main>
+    <Footer />
+  </>
+);
 
 export default App;
-
