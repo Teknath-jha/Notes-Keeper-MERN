0b86e0c3440a5fe9572eb58434ac87f6b114a484
Teknath <jhateknath64@gmail.com>
2023-01-03 15:12:45 +0530

    Added MyNotes page with Dummy DataBase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,21 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import LandingPage from "./screens/LandingPage/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MyNotes from "./screens/MyNotes/MyNotes";
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <Header />
-    <main >
-      <LandingPage />
+    <main>
+      <Routes>
+        <Route path="/" element={ <LandingPage />} exact />
+
+        <Route path="/mynotes" element={<MyNotes />} />
+      </Routes>
     </main>
     <Footer />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
