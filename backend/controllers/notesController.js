4f17363534cdf0a5839e995a22eb8cb3298e8d51
Teknath <jhateknath64@gmail.com>
2023-01-06 11:06:54 +0530

    UPDATE & DELETE api added

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -21,4 +21,54 @@ const createNote = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getNotes, createNote };
+const getNoteById = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).json({ message: "Note not found" });
+  }
+});
+
+const updateNote = asyncHandler(async (req, res) => {
+  const { title, content, category } = req.body;
+  const note = await Note.findById(req.params.id);
+
+  console.log(user);
+
+  if ((note.user.toString() !== req, user._id.toString())) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    note.title = title;
+    note.content = content;
+    note.category = category;
+
+    const updatedNote = await note.save();
+    res.json(updatedNote);
+  } else {
+    res.status(404);
+    throw new Error("Note not found");
+  }
+});
+
+const deleteNote = asyncHandler(async (req, res) => {
+  const note = await Note.findById(req.params.id);
+
+  if ((note.user.toString() !== req, user._id.toString())) {
+    res.status(401);
+    throw new Error("You can't perform this action");
+  }
+
+  if (note) {
+    await note.remove();
+    res.json({ message: "Note deleted" });
+  } else {
+    res.status(404).json({ message: "Note not found" });
+  }
+});
+
+module.exports = { getNotes, createNote, getNoteById, updateNote, deleteNote };
