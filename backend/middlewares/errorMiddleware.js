1891f59a0628ce79bfa4fe9ddb4f6d7d47445f40
Teknath <jhateknath64@gmail.com>
2023-01-08 20:12:12 +0530

    deployment fix

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,5 +1,6 @@
 //if url not found
 const notFound = (req, res, next) => {
+  console.log(req);
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
@@ -9,7 +10,7 @@ const notFound = (req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   res.status(statusCode);
-  res.json({ 
+  res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
